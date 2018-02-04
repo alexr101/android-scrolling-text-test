682a8816cb6ad46a002b5110ea2eb389f22a7054
alexr101 <alex_r_101@hotmail.com>
2018-02-04 01:09:26 -0500

    add http request in main activity and response handler

--- app/src/main/java/com/alexpadilla/scrollingtext/HttpUtils.java
+++ app/src/main/java/com/alexpadilla/scrollingtext/HttpUtils.java
@@ -9,22 +9,26 @@ import com.loopj.android.http.RequestParams;
  */
 
 public class HttpUtils {
-    private static final String BASE_URL = "http://api.twitter.com/1/";
+    private static final String BASE_URL = "https://api.cryptonator.com/api/ticker/";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
+    // Get with BASE_URL
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
+    // Post with BASE_URL
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
+    // Get by parameter url
     public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(url, params, responseHandler);
     }
 
+    // Post by parameter url
     public static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.post(url, params, responseHandler);
     }
--- app/src/main/java/com/alexpadilla/scrollingtext/MainActivity.java
+++ app/src/main/java/com/alexpadilla/scrollingtext/MainActivity.java
@@ -4,7 +4,14 @@ package com.alexpadilla.scrollingtext;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import com.loopj.android.http.*;
+
+import com.alexpadilla.scrollingtext.HttpUtils;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +20,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestParams rp = new RequestParams();
+        rp.add("username", "aaa"); rp.add("password", "aaa@123");
+
+        HttpUtils.get("btc-usd", rp, new JsonHttpResponseHandler() {
+            
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                Log.d("asd", "---------------- this is response : " + response);
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+
+            }
+        } )
+
 
         TextView marque = (TextView) this.findViewById(R.id.marque_scrolling_text);
         marque.setSelected(true);
